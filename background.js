c5ceb8427facf3caada301b71ff7b2d8f3c3db1a
Liam Wiltshire <liam@w.iltshi.re>
2020-11-17 16:44:18 +0000

    Also update labels (#1)
    * Also update labels
    
    Allows colours to work in un-expanded mode
    
    * Reduce number of items to check, only run on Trello tabs
    
    * Set timeout to 0

--- background.js
+++ background.js
@@ -1,23 +1,28 @@
 
 chrome.tabs.onUpdated.addListener(function() {
     chrome.tabs.query({"active": true, "lastFocusedWindow": true}, function(tabs) {
-      chrome.tabs.executeScript(tabs[0].id, {
-        "code": `
+      if (tabs[0].title.indexOf("Trello") !== -1) {
+	      chrome.tabs.executeScript(tabs[0].id, {
+            "code": `
 
-          setInterval(function() {
-            var labels = document.getElementsByClassName("card-label");
+              setInterval(function() {
+                var labels = document.querySelectorAll(".card-label[title*='#']");
 
-            for (var i = 0, len = labels.length, color; i < len; i++) {
-              if (labels[i].innerText.indexOf("#") === 0 && labels[i].innerText.indexOf(":") > 0) {
-                color = labels[i].innerText.split(":")[0];
-                labels[i].style.backgroundColor = color;
+                for (var i = 0, len = labels.length, color; i < len; i++) {
+                  if (labels[i].title.indexOf(":") > 0) {
+                    color = labels[i].title.split(":")[0];
+                    labels[i].style.backgroundColor = color;
 
-                labels[i].innerText = labels[i].innerText.substring(color.length + 1);
-              }
-            }
-          }, 0);
+                    labels[i].title = labels[i].title.substring(color.length + 1);
+                    if (labels[i].innerText != "") {
+                        labels[i].innerText = labels[i].innerText.substring(color.length + 1);                
+                    }
+                  }        
+                }
+              }, 0);
 
-        `
-      });
+		`
+	      });
+      }
     });
 });
